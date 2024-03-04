8a8312147fdccccef52fc781f8b08dd910162330
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-04 11:55:38 +0300

    Update ESLint config and add a header component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,14 @@ import { hamburger } from "../assets/icons";
 
 export const Nav = () => {
   return (
-    <header>
-      <nav>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
         <a href="">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
+        <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+          <li></li>
+        </ul>
       </nav>
     </header>
   );
