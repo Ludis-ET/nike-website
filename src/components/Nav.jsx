7fb7f9613c6b59478aedf5cc4e44a5691142a032
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-04 12:27:41 +0300

    Added navigation links and constants for the e-commerce website.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
 
 export const Nav = () => {
   return (
@@ -9,8 +10,20 @@ export const Nav = () => {
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
         <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
-          <li></li>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-monserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
+        <div className="hidden max-lg:block cursor-pointer">
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
       </nav>
     </header>
   );
